26bddfe03f860c3001ae9784d4a21986152bd7ff
Nazarii Gudzovatyi <nazarii@enapps.co.uk>
2015-10-26 23:16:09 +0200

    Internationalization and localization

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -2,24 +2,28 @@
 from selenium import webdriver
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
- 
- 
+from django.utils.translation import activate
+from datetime import date
+from django.utils import formats
+
+
 class HomeNewVisitorTest(StaticLiveServerTestCase):
- 
+
     def setUp(self):
+        activate('en')
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
- 
+
     def tearDown(self):
         self.browser.quit()
- 
+
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)
- 
+
     def test_home_title(self):
         self.browser.get(self.get_full_url("home"))
         self.assertIn("TaskBuster", self.browser.title)
- 
+
     def test_h1_css(self):
         self.browser.get(self.get_full_url("home"))
         h1 = self.browser.find_element_by_tag_name("h1")
@@ -30,3 +34,22 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
+
+    def test_internationalization(self):
+        for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+                            ('uk', u'Вітаю на TaskBuster!')]:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            h1 = self.browser.find_element_by_tag_name("h1")
+            self.assertEqual(h1.text, h1_text)
+
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'uk']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id("local-date")
+            non_local_date = self.browser.find_element_by_id("non-local-date")
+            self.assertEqual(formats.date_format(today, use_l10n=True),
+                                  local_date.text)
+        self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
